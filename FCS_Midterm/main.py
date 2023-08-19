dc6aa7937cdc8f693383b6b4527a531bdd2d4fbf
RamiKaafarani <kaafaranirami@gmail.com>
2023-08-19 20:09:09 +0300

    Midterm

---
+++ FCS_Midterm/main.py
@@ -0,0 +1,33 @@
+from read_text import get_names
+path = "Employess.txt"
+data=get_names(path)
+count = 0
+correct_username = False
+while count < 5:
+    login_username=input("Please enter your Username")
+    login_password=input("Please enter your Password")
+    if login_username == "Admin" and login_password == "Admin123123":
+        print("Admin Menu")
+        break
+    elif login_username != "Admin" and login_password == "" and login_username == "":
+        for i in data:
+            if login_username == i["username"]:
+                print("Employee Menu")
+                correct_username = True
+                break
+        if correct_username == True:
+            break
+        else:
+            count += 1
+            print(count)
+    else:
+        count +=1
+        if count == 5:
+            print("You have been blocked")
+
+
+
+
+
+
+
---
+++ FCS_Midterm/read_text.py
@@ -0,0 +1,14 @@
+# https://www.pythontutorial.net/python-basics/python-read-text-file/ (Used to read a file)
+# https://www.w3schools.com/python/ref_string_split.asp (Used to split the string into list)
+# https://www.w3schools.com/python/ref_file_readlines.asp#:~:text=The%20readlines()%20method%20returns,no%20more%20lines%20are%20returned (what does readline do)
+def get_names(path):
+    with open(path) as f:
+        employees = f.readlines()
+    data = []
+    for employee in employees:
+        employees_split = employee.split(",")
+        id, username, date, gender, salary = employees_split
+        info = {"id" : id , "username" : username ,"date" : date ,"gender" : gender,"salary" : salary}
+        data.append(info)
+    return data
+
